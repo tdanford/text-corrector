03fbc51f7ded163b9463be80f6df1de5b575bf7a
Timothy Danford <tdanford@gmail.com>
2010-10-06 14:07:04 -0400

    Logging, accessors.
    Added support for carrying a logger through with the SimpleInMemoryAdaptiveMatcher class.  Also added several accessors, such as getMatches(), addMatch(), removeMatch(), and clear(), all intended for other classes which will use this class as a proxy.

--- src/org/sc/annotator/adaptive/SimpleInMemoryAdaptiveMatcher.java
+++ src/org/sc/annotator/adaptive/SimpleInMemoryAdaptiveMatcher.java
@@ -10,15 +10,49 @@ import java.util.TreeMap;
 
 import org.sc.annotator.adaptive.exceptions.MatcherCloseException;
 import org.sc.annotator.adaptive.exceptions.MatcherException;
+import org.slf4j.Logger;
 
 public class SimpleInMemoryAdaptiveMatcher implements AdaptiveMatcher {
 	
 	private Map<String,Set<Match>> matches;
 	
-	public SimpleInMemoryAdaptiveMatcher() { 
+	private Logger logger;
+	
+	public SimpleInMemoryAdaptiveMatcher(Logger logger) {
+		this.logger = logger;
 		matches = new TreeMap<String,Set<Match>>();
 	}
+	
+	/**
+	 * Package-only access to the internal Match store.
+	 * 
+	 * @return
+	 */
+	Map<String,Set<Match>> getMatches() { 
+		return matches;
+	}
+	
+	public void clear() { 
+		matches.clear();
+	}
+	
+	public void addMatch(Match m) { 
+		if(!matches.containsKey(m.match())) { 
+			matches.put(m.match(), new LinkedHashSet<Match>());
+		}
+		matches.get(m.match()).add(m);
+	}
+	
+	public void removeMatch(Match m) { 
+		matches.get(m.match()).remove(m);
+		if(matches.get(m.match()).isEmpty()) { 
+			matches.remove(m.match());
+		}
+	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public Collection<Match> findMatches(Context c, String blockText) {
 		LinkedList<Match> ms = new LinkedList<Match>();
 		
@@ -38,6 +72,9 @@ public class SimpleInMemoryAdaptiveMatcher implements AdaptiveMatcher {
 		return ms;
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	public Context registerMatch(Match m) {
 		String matchText = m.match();
 		if(!matches.containsKey(matchText)) { matches.put(matchText, new LinkedHashSet<Match>()); }
@@ -60,6 +97,11 @@ public class SimpleInMemoryAdaptiveMatcher implements AdaptiveMatcher {
 		return m.context();
 	}
 
+	/**
+	 * @inheritDoc
+	 * 
+	 * This method always succeeds, for the in-memory adaptive matcher.
+	 */
 	public void close() throws MatcherCloseException {
 	} 
 	
