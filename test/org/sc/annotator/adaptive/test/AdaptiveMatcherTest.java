5502737cbf9526f8e89de670dd4c35a51ad73c5c
Timothy Danford <tdanford@gmail.com>
2010-10-06 10:18:19 -0400

    JUnit tests for the WebClient adaptive matcher.

---
+++ test/org/sc/annotator/adaptive/test/AdaptiveMatcherTest.java
@@ -0,0 +1,43 @@
+package org.sc.annotator.adaptive.test;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.regex.Pattern;
+
+import org.junit.*;
+import org.sc.annotator.adaptive.*;
+import org.sc.annotator.adaptive.client.WebClient;
+
+import static org.junit.Assert.*;
+
+public class AdaptiveMatcherTest {
+
+	private AdaptiveMatcher matcher;
+	
+	private Match m1;
+	
+	@Before 
+	public void setup() { 
+		matcher = new WebClient("http://localhost:8080/matcher");
+		
+	}
+	
+	@Test 
+	public void test1() throws IOException {  
+		m1 = new Match(new Context("foo bar"), "xxx", "yyy");
+		
+		Context c = matcher.registerMatch(m1);
+		assertEquals(c, m1.context());
+
+		Collection<Match> matches = matcher.findMatches(new Context("foo bar"), "xxx");
+		assertTrue(matches.contains(m1));
+		
+		Match m2 = new Match(new Context("foo grok"), "xxx", "yyy");
+		
+		Context c2 = matcher.registerMatch(m2);
+		assertEquals(c2, new Context("foo"));
+		
+		matches = matcher.findMatches(new Context("foo quux"), "xxx");
+	}	
+}
