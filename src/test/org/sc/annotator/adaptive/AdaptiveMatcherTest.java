7744e5100dca5452ab81f6ace0b51e24ba8e6e3f
Timothy Danford <tdanford@gmail.com>
2010-10-20 17:09:33 -0400

    Revised with assertThat()-style notation.
    Also, made it test a local AdaptiveMatcher.

--- src/test/org/sc/annotator/adaptive/AdaptiveMatcherTest.java
+++ src/test/org/sc/annotator/adaptive/AdaptiveMatcherTest.java
@@ -6,39 +6,41 @@ import java.util.*;
 import java.util.regex.Pattern;
 
 import org.junit.*;
+
 import org.sc.annotator.adaptive.*;
 import org.sc.annotator.adaptive.client.WebClient;
 import org.sc.annotator.adaptive.exceptions.MatcherCloseException;
 import org.sc.annotator.adaptive.exceptions.MatcherException;
 
 import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.matchers.JUnitMatchers.*;
 
 public class AdaptiveMatcherTest {
 
 	private AdaptiveMatcher matcher;
 	
-	private Match m1;
-	
 	@Before 
 	public void setup() { 
-		matcher = new WebClient("http://localhost:8080/matcher");
-		
+		//matcher = new WebClient("http://localhost:8080/matcher");
+		matcher = new SimpleInMemoryAdaptiveMatcher();
 	}
 	
 	@Test 
 	public void test1() throws IOException, MatcherException {  
-		m1 = new Match(new Context("foo bar"), "xxx", "yyy");
-		
+		Match m1 = new Match(new Context("foo bar"), "xxx", "yyy");
 		Context c = matcher.registerMatch(m1);
-		assertEquals(c, m1.context());
+		
+		assertThat(c, is(equalTo(m1.context())));
 
 		Collection<Match> matches = matcher.findMatches(new Context("foo bar"), "xxx");
-		assertTrue(matches.contains(m1));
+
+		assertThat(matches, hasItem(m1));
 		
 		Match m2 = new Match(new Context("foo grok"), "xxx", "yyy");
-		
 		Context c2 = matcher.registerMatch(m2);
-		assertEquals(c2, new Context("foo"));
+		
+		assertThat(c2, is(equalTo(new Context("foo"))));
 		
 		matches = matcher.findMatches(new Context("foo quux"), "xxx");
 	}	
